8e3d095f4b70d90f24c0e63c802a1a6fdbcfa831
Tournier Quentin <qttournier@gmail.com>
2024-11-27 16:35:38 +0100

    fix: remove unuse variable

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,6 +1,5 @@
 class_name Player extends CharacterBody2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var ray_cast: RayCast2D = $RayCast2D
 
