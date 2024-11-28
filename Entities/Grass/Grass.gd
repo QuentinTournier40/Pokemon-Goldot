d2d98ad8f0bb74df5bba40c1a31150da72f18c37
Tournier Quentin <qttournier@gmail.com>
2024-11-28 19:07:01 +0100

    refacto: grass animation

--- Entities/Grass/Grass.gd
+++ Entities/Grass/Grass.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
-#@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-@onready var foreground_3: Sprite2D = $Foreground3
-@onready var foreground_4: Sprite2D = $Foreground4
+@onready var foreground_bottom: Sprite2D = $ForegroundBottom
 @onready var moving_grass: Sprite2D = $MovingGrass
 
 func _ready() -> void:
@@ -12,10 +9,8 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		moving_grass.set_visible(true)
-		foreground_3.set_z_index(26)
-		foreground_4.set_z_index(26)
+		foreground_bottom.set_z_index(26)
 
 func _on_body_exited(body: Node2D) -> void:
-	foreground_3.set_z_index(1)
-	foreground_4.set_z_index(1)
+	foreground_bottom.set_z_index(1)
 	moving_grass.set_visible(false)
