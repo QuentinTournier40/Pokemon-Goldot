8a7322d21376b04b1dcd62109e585bdd8edf7711
Tournier Quentin <qttournier@gmail.com>
2024-11-29 17:43:27 +0100

    feat: better teleport

--- Common/TeleportManagement/Portal2D.gd
+++ Common/TeleportManagement/Portal2D.gd
@@ -1,7 +1,10 @@
 class_name Portal2D extends Area2D
 
 @export_file("*.tscn") var next_scene_path
-@export var teleport_to: Vector2 = Vector2.ZERO
+@export var teleport_to: String
+
+func _ready() -> void:
+	self.body_entered.connect(_on_body_entered)
 
 func teleport(next_scene: PackedScene = load(next_scene_path)):
 	var new_scene = next_scene.instantiate()
@@ -9,18 +12,19 @@ func teleport(next_scene: PackedScene = load(next_scene_path)):
 	var current_scene = get_tree().current_scene
 	get_tree().get_root().add_child(new_scene)
 	get_tree().current_scene = new_scene
-	
+
 	current_scene.queue_free()
 
-	teleport_player_in_new_scene(new_scene)
+	teleport_player_in_new_scene()
 
-func teleport_player_in_new_scene(new_scene):
-	var player = new_scene.find_child("Player")
-	if player:
-		player.global_position = teleport_to
+func teleport_player_in_new_scene():
+	var player = get_tree().current_scene.find_child("Player")
+	var point = get_tree().current_scene.find_child(teleport_to)
+	if player and point:
+		player.global_position = point.global_position
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name != "Player":
 		return
 
-	teleport()
+	call_deferred("teleport")
